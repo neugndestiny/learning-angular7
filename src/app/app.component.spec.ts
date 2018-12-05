4c1c14e69386def616c20897ed32cc4f7f27dbf8
neugndestiny <neungdestiny12@gmail.com>
2018-12-05 22:10:43 +0700

    simple data binding angular test by using karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,38 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '../../node_modules/@angular/platform-browser';
+import { FormsModule } from '../../node_modules/@angular/forms';
+import { DebugElement } from '../../node_modules/@angular/core';
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let de: DebugElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        BrowserModule,
+        FormsModule
+      ]
     }).compileComponents();
   }));
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    de = fixture.debugElement;
+
+    fixture.detectChanges();
+  }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'angular7'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular7');
+  it(`should have as title 'Hello World'`, async(() => {
+    expect(app.title).toEqual('Hello World');
   }));
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular7!');
+    expect(de.nativeElement.querySelector('h1').textContent).toEqual('Hello World');
   }));
 });
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -20,6 +20,9 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
+    browserConsoleLogOptions: {
+      level: 'log'
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
